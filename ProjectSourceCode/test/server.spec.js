825dbeaa61955eb60913d97976f523fd2c227b7d
socho1 <joshmyeongkim@gmail.com>
2025-04-10 13:25:49 -0600

    fixed test cases

--- ProjectSourceCode/test/server.spec.js
+++ ProjectSourceCode/test/server.spec.js
@@ -52,20 +52,33 @@ describe('Register', () => {
     });
 });
 
-// describe('Testing Redirect', () => {
-//   // Sample test case given to test /test endpoint.
-//   it('\test route should redirect to /login with 302 HTTP status code', done => {
-//     chai
-//       .request(server)
-//       .get('/test')
-//       .end((err, res) => {
-//         res.should.have.status(302); // Expecting a redirect status code
-//         res.should.redirectTo(/^.*127\.0\.0\.1.*\/login$/); // Expecting a redirect to /login with the mentioned Regex
-//         done();
-//       });
-//   });
-// });
+describe('Testing login through previously made user', () => {
+  // Sample test case given to test /test endpoint.
+  it('test if /register correctly adds user and redirects to login', done => {
+    chai
+      .request(server)
+      .post('/login')
+      .send({username: 'heyheyhey', password: '12345'})
+      .end((err, res) => {
+        res.should.have.status(200); 
+        done();
+      });
+  });
+});
 
+describe('Testing if maps loads', () =>{
+     // Sample test case given to test /test endpoint.
+  it('test /maps route renders properly', done => {
+    chai
+      .request(server)
+      .get('/maps') // for reference, see lab 8's login route (/login) which renders home.hbs
+      .end((err, res) => {
+        res.should.have.status(200); // Expecting a success status code
+        res.should.be.html; // Expecting a HTML response
+        done();
+      });
+  }); 
+})
 describe('Testing Render', () => {
   // Sample test case given to test /test endpoint.
   it('test "/login" route should render with an html response', done => {
