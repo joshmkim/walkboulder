bc0d128aa0cdd9c524fee7c9541119667c9170da
Isabelle Lindfors <Isabelle.Lindfors@colorado.edu>
2025-04-10 09:45:30 -0600

    Start adding achievement related sql, images are in the procress of being made - incomplete

---
+++ ProjectSourceCode/init_data/achievements.sql
@@ -0,0 +1,12 @@
+INSERT INTO achievements (achievements_id, achievements_url, achievements_caption)
+    VALUES
+    (1, '', 'Record your first walk'),
+    (2, '', 'Add your first friend'),
+    (3, '', 'Leave your first review'),
+    (4, '', 'Use WalkBoulder for 1 week'),
+    (5, '', 'Walk a total of X miles / Y meters'),
+    (6, '', 'Have Z friends'),
+    (7, '', 'First walk with a buddy'),
+    (8, '', 'Use WalkBoulder for 1 month'),
+    (9, '', 'Walk on a pre-set trail'),
+    (10, '', 'Create your own route');
